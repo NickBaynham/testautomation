09bb1a73a0d179ab6f7a726b6410f8be1c99f616
FLI\nbaynham <nbaynham@flhi.com>
2019-12-23 18:39:00 -0500

    connect to grid

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,12 +1,22 @@
 package utils;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import javax.xml.bind.annotation.XmlType;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static utils.DriverType.valueOf;
 import static utils.DriverType.FIREFOX;
 
 public class DriverFactory {
+    private static final DriverType DEFAULT_DRIVER_TYPE = FIREFOX;
+    private final String browser = System.getProperty("browser", DEFAULT_DRIVER_TYPE.name().toUpperCase());
+    private final boolean useRemoteWebDriver = Boolean.getBoolean("remoteDriver");
     private RemoteWebDriver driver;
     private DriverType driverType;
     private final String operatingSystem = System.getProperty("os.name").toUpperCase();
@@ -25,14 +35,14 @@ public class DriverFactory {
         this.driverType = driverType;
     }
 
-    public RemoteWebDriver getDriver() {
+    public RemoteWebDriver getDriver() throws MalformedURLException {
         if (null == driver) {
             instantiateWebDriver(driverType);
         }
         return driver;
     }
 
-    private void instantiateWebDriver(DriverType driverType) {
+    private void instantiateWebDriver(DriverType driverType) throws MalformedURLException {
         System.out.println(" ");
         System.out.println("Local Operating System: " +
                 operatingSystem);
@@ -41,9 +51,25 @@ public class DriverFactory {
         System.out.println("Selected Browser: " +
                 driverType);
         System.out.println(" ");
-        DesiredCapabilities desiredCapabilities = new
-                DesiredCapabilities();
-        driver = driverType.getWebDriverObject(desiredCapabilities);
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        if (useRemoteWebDriver) {
+            URL seleniumGridURL = new URL(System.getProperty("gridURL"));
+            String desiredBrowserVersion = System.getProperty("desiredBrowserVersion");
+            String desiredPlatform = System.getProperty("desiredPlatform");
+
+            if (null != desiredPlatform && !desiredPlatform.isEmpty())
+            {
+                desiredCapabilities.setPlatform(Platform.valueOf(desiredPlatform.toUpperCase()));
+            }
+
+            if (null != desiredBrowserVersion &&
+                    !desiredBrowserVersion.isEmpty()) {
+                desiredCapabilities.setVersion(desiredBrowserVersion);
+            }
+            driver = new RemoteWebDriver(seleniumGridURL, desiredCapabilities);
+        } else {
+            driver = driverType.getWebDriverObject(desiredCapabilities);
+        }
     }
 
     public void quit() {
